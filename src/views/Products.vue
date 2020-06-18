447bab768edfe6c5d897374712d6ccbe02c66278
Marek Giełda <marek.gielda@tsh.io>
2020-06-19 00:49:59 +0200

    Logo has been added

---
+++ src/views/Products.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="productsContainer">
+    <header>
+      <Logo />
+    </header>
+    <section></section>
+  </div>
+</template>
+
+<script>
+import Logo from "@/components/common/Logo.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.productsContainer {
+}
+</style>
