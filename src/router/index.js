911e38a7982a21309e2f7d1b19a5c8fbea0427f8
Marek Giełda <marek.gielda@tsh.io>
2020-07-02 07:01:08 +0200

    Comment remove from router file

--- src/router/index.js
+++ src/router/index.js
@@ -13,17 +13,12 @@ const routes = [
   {
     path: "/login",
     name: "Login",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue")
+    component: () => import("../views/Login.vue")
   },
   {
     path: "/passwordRecovery",
     name: "PasswordRecovery",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/PasswordRecovery.vue")
+    component: () => import("../views/PasswordRecovery.vue")
   }
 ];
 
